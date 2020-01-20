341c9ee61057d65586647f5b8b97bc4282d8d295
Aleksandr Mishchenko <AMishchenko@luxoft.com>
2020-01-20 16:31:02 +0200

    delete SDLRPCV1 and JSONRPC parsers

--- InterfaceParser/test/runner.py
+++ InterfaceParser/test/runner.py
@@ -9,8 +9,6 @@ from unittest import TestLoader, TestSuite, TextTestRunner
 sys.path.append(Path(__file__).absolute().parents[1].as_posix())
 
 try:
-    from parsers_test.test_hmi_api_rpc import TestJSONRPCVParser
-    from parsers_test.test_sdl_rpc_v1 import TestSDLRPCV1Parser
     from parsers_test.test_sdl_rpc_v2 import TestSDLRPCV2Parser
     from test_model import TestInterfaceModel
     from test_code_format_and_quality import CodeFormatAndQuality
@@ -21,8 +19,6 @@ except ModuleNotFoundError as error:
 if __name__ == '__main__':
     SUITE = TestSuite()
 
-    SUITE.addTests(TestLoader().loadTestsFromTestCase(TestJSONRPCVParser))
-    SUITE.addTests(TestLoader().loadTestsFromTestCase(TestSDLRPCV1Parser))
     SUITE.addTests(TestLoader().loadTestsFromTestCase(TestSDLRPCV2Parser))
     SUITE.addTests(TestLoader().loadTestsFromTestCase(TestInterfaceModel))
     SUITE.addTests(TestLoader().loadTestsFromTestCase(CodeFormatAndQuality))
